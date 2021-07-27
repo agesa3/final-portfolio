5e6aed403f931a9e9888e3291726e81883050ce7
agesa3 <collinsagesa2000@gmail.com>
2021-07-28 00:51:51 +0300

    more updates

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-
-
+import Particles from 'react-particles-js';
 
 function App() {
   return (
     <>
+      <Particles
+        params={{
+         particles: {
+          number: {
+            value: 30,
+            density: {
+              enabled: true,
+              value_area: 900,
+            },
+          },
+          shape: {
+            type:"circle",
+            stroke:{
+              width:6,
+              color:"#f9ab00"
+            }
+          }
+         }
+        }}
+      />
       <Navbar />
-      <Header /> 
+      <Header />
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Typed from 'react-typed'
 
 const Header = () => {
   return (
     <div className="header-wrapper">
       <div className="main-info">
         <h1>Web Development and websites promotions</h1>
+        <Typed 
+        className="typed-text" 
+        strings={["Web Development","Android Development","Blockchain Development"]}
+        typeSpeed={40}
+        backSpeed={60}
+        loop
+        />
+        <a  className="btn-main-offer" href="https://">Contact Me</a>
       </div>
     </div>
   );
